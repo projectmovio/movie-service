ebf9815b7bdf7b0adf8ef7537a4ac6e86374b90b
Michal Sadowski <misad90@gmail.com>
2017-10-28 19:31:35 +0200

    Remove service.py

--- service/server.py
+++ service/server.py
@@ -1,10 +1,9 @@
 from flask import Flask
 
-from service import Service
+from api.tmdb import TmdbApi
 
 app = Flask(__name__)
-service = Service()
-
+tmdb_api = TmdbApi()
 
 @app.route("/")
 def hello():
