b17638f0a47b71109929f30dd1ea51612baf4d9d
Michal Sadowski <misad90@gmail.com>
2017-10-30 23:40:16 +0100

    Template swagger file and proper $refs

--- service/server.py
+++ service/server.py
@@ -3,28 +3,7 @@ from flask import Flask, request, jsonify
 from api.tmdb import TmdbApi
 
 app = Flask(__name__)
-swagger_template = {
-  "swagger": "2.0",
-  "info": {
-    "title": "Movio movie service api",
-    "description": "API to get and update movie information",
-    "contact": {
-      "responsibleOrganization": "Michal Sadowski",
-      "responsibleDeveloper": "Michal Sadowski",
-      "email": "michcio90po@gmail.com",
-      "url": "test",
-    },
-    "termsOfService": "test",
-    "version": "0.0.0"
-  },
-  "host": "localhost:5000",  # overrides localhost:5000
-  "basePath": "/api",  # base bash for blueprint registration
-  "schemes": [
-    "http",
-    "https"
-  ]
-}
-Swagger(app, template=swagger_template)
+Swagger(app, template_file='swagger/template.yml')
 
 tmdb_api = TmdbApi()
 
