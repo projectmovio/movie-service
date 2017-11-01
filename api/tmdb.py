9ea390b4096a63e638bedbb5c5040ccc9c1e7567
Michal Sadowski <misad90@gmail.com>
2017-11-01 22:36:56 +0100

    Dont get tmdb key twice

--- api/tmdb.py
+++ api/tmdb.py
@@ -9,7 +9,6 @@ log = Log().get_logger(__name__)
 class TmdbApi(BaseApi):
     def __init__(self):
         super(TmdbApi, self).__init__()
-        self.config = self.config["tmdb"]
         self.headers = {"Authorization": self.config["user_token"]}
         self.config["configuration"] = self.configuration()
         log.debug("TMDB image base_url: {}".format(
