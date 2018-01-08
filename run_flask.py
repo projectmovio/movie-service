a96ea7747c4744af5475dc9710d65874143ff7ca
Michal Sadowski <misad90@gmail.com>
2018-01-09 00:04:02 +0100

    Not needed stdout/stderr Popen params

--- run_flask.py
+++ run_flask.py
@@ -1,7 +1,6 @@
 import os
-import sys
 from subprocess import Popen
 
 os.environ["FLASK_APP"] = "service/server.py"
-flask_ps = Popen(["flask", "run", "--host=0.0.0.0"], stdout=sys.__stdout__, stderr=sys.__stderr__)
+flask_ps = Popen(["flask", "run", "--host=0.0.0.0"])
 flask_ps.communicate()
