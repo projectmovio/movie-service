6d750d45b29c85d214341fd4b57e95da1cc38124
Michal Sadowski <michal.sadowski@axis.com>
2018-04-06 23:43:20 +0200

    Print /movies headers

--- service/server.py
+++ service/server.py
@@ -1,7 +1,6 @@
 import logging
 
 from api.tmdb import TmdbApi
-from flasgger import swag_from, Swagger
 from flask import Flask, request
 
 log = logging.getLogger("service")
@@ -9,14 +8,13 @@ logging.basicConfig(level=logging.DEBUG, format="%(asctime)s - %(name)s - %(leve
 logging.getLogger("urllib3").setLevel("WARNING")
 
 app = Flask(__name__)
-Swagger(app, template_file='swagger/template.yml')
 
 tmdb_api = TmdbApi()
 
 
 @app.route("/movies", methods=["get"])
-@swag_from("swagger/movies.yml")
 def movies():
+    log.debug("Headers: %s", request.headers)
     if "search" in request.args:
         search = request.args.get("search")
         return tmdb_api.search(search)
