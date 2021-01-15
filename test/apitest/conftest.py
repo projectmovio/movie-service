073434643835ca7561a420d51c080579dbcdc688
fulder <misad90@gmail.com>
2021-01-16 00:32:35 +0100

    Add unittests

---
+++ test/apitest/conftest.py
@@ -0,0 +1,6 @@
+import os
+
+API_URL = "api.movie.moshan.tv"
+BASE_HEADERS = {
+    "Authorization": os.getenv("TOKEN")
+}
---
+++ test/unittest/test_movies_db.py
@@ -0,0 +1,19 @@
+import pytest
+
+
+def test_get_show_by_tvmaze_id_not_found(mocked_movies_db):
+    mocked_movies_db.table.query.return_value = {
+        "Items": []
+    }
+
+    with pytest.raises(mocked_movies_db.NotFoundError):
+        mocked_movies_db.get_movie_by_api_id("tmdb", "123")
+
+
+def test_get_show_by_id_not_found(mocked_movies_db):
+    mocked_movies_db.table.get_item.return_value = {
+        "Items": []
+    }
+
+    with pytest.raises(mocked_movies_db.NotFoundError):
+        mocked_movies_db.get_movie_by_id("123")
